16fa98e10a877cf8963d24f6093a56e8896e42bf
Sidharth5117 <sidharth2136@gmail.com>
2020-04-22 12:38:03 +0530

    Created migration and model file for user

--- rails/app/db/schema.rb
+++ rails/app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_193525) do
+ActiveRecord::Schema.define(version: 2020_04_22_070336) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2020_04_18_193525) do
     t.string "moral"
     t.string "author"
     t.datetime "created_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
