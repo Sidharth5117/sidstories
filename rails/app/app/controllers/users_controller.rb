194af4697e5a86394ae801e33bbe467155b37ba3
Sidharth5117 <sidharth2136@gmail.com>
2020-04-24 17:47:59 +0530

    Restricted actions using controller

--- rails/app/app/controllers/users_controller.rb
+++ rails/app/app/controllers/users_controller.rb
@@ -1,63 +1,56 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   before_action :require_same_user, only: [:edit, :update]
-  # GET /users
-  # GET /users.json
+
+
   def index
     @user = User.paginate(page: params[:page], per_page: 3)
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
+ def show
   end
 
-  # GET /users/new
+
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
+
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
+
       if @user.save
-        format.html { redirect_to @user, flash[:success]= 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+      redirect_to @user
+      flash[:success]= "Welcome to SidStories #{@user.username}" 
+      redirect_to user_path(@user) 
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+       render "new" 
       end
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
+  
   def update
-    respond_to do |format|
+    
       if @user.update(user_params)
-        format.html { redirect_to @user, flash[:success]= 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+      flash[:success]= "Your Account was successfully updated."
+      redirect_to stories_path  
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+       render "edit"
+      
       end
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, flash[:danger] =  'User was successfully destroyed.' }
-      format.json { head :no_content }
+ flash[:danger] =  "User and all stories created by user have been successfully destroyed." 
+      redirect_to users_path
     end
   end
 
